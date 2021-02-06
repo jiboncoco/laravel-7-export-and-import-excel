2978c1f6e18abee6e81bcdfdd08d97d5447134d7
Achmad Fauzi <achmadfauzi@live.com>
2021-02-07 01:57:56 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/siswa', 'SiswaController@index');
+Route::get('/siswa/export_excel', 'SiswaController@export_excel');
+Route::post('/siswa/import_excel', 'SiswaController@import_excel');
